30cd6c2b17a8218236af0df632b4176ad87f31ff
Quavertron <user@example.com>
2026-03-28 18:15:13 -0400

    removed exta tag handling

--- lib/philomena/images/tag_validator.ex
+++ lib/philomena/images/tag_validator.ex
@@ -7,44 +7,15 @@ defmodule Philomena.Images.TagValidator do
 
     changeset
     |> validate_tag_input(tags)
-    |> set_rating_changed()
-  end
-
-  defp set_rating_changed(changeset) do
-    added_tags = changeset |> get_field(:added_tags) |> extract_names()
-    removed_tags = changeset |> get_field(:removed_tags) |> extract_names()
-    ratings = all_ratings()
-
-    added_ratings = MapSet.intersection(ratings, added_tags) |> MapSet.size()
-    removed_ratings = MapSet.intersection(ratings, removed_tags) |> MapSet.size()
-
-    put_change(changeset, :ratings_changed, added_ratings + removed_ratings > 0)
+    |> put_change(:ratings_changed, false)
   end
 
   defp validate_tag_input(changeset, tags) do
     tag_set = extract_names(tags)
-    rating_set = ratings(tag_set)
 
     changeset
     |> validate_number_of_tags(tag_set, 1)
     |> validate_bad_words(tag_set)
-    |> validate_safe(rating_set)
-    |> validate_sexual_exclusion(rating_set)
-    |> validate_horror_exclusion(rating_set)
-  end
-
-  defp ratings(tag_set) do
-    safe = MapSet.intersection(tag_set, safe_rating())
-    sexual = MapSet.intersection(tag_set, sexual_ratings())
-    horror = MapSet.intersection(tag_set, horror_ratings())
-    gross = MapSet.intersection(tag_set, gross_rating())
-
-    %{
-      safe: safe,
-      sexual: sexual,
-      horror: horror,
-      gross: gross
-    }
   end
 
   defp validate_number_of_tags(changeset, tag_set, num) do
@@ -70,53 +41,10 @@ defmodule Philomena.Images.TagValidator do
     end
   end
 
-  defp validate_has_rating(changeset, %{safe: s, sexual: x, horror: h, gross: g}) do
-    if MapSet.size(s) > 0 or MapSet.size(x) > 0 or MapSet.size(h) > 0 or MapSet.size(g) > 0 do
-      changeset
-    else
-      add_error(changeset, :tag_input, "must contain at least one rating tag")
-    end
-  end
-
-  defp validate_safe(changeset, %{safe: s, sexual: x, horror: h, gross: g}) do
-    if MapSet.size(s) > 0 and (MapSet.size(x) > 0 or MapSet.size(h) > 0 or MapSet.size(g) > 0) do
-      add_error(changeset, :tag_input, "may not contain any other rating if safe")
-    else
-      changeset
-    end
-  end
-
-  defp validate_sexual_exclusion(changeset, %{sexual: x}) do
-    if MapSet.size(x) > 1 do
-      add_error(changeset, :tag_input, "may contain at most one sexual rating")
-    else
-      changeset
-    end
-  end
-
-  defp validate_horror_exclusion(changeset, %{horror: h}) do
-    if MapSet.size(h) > 1 do
-      add_error(changeset, :tag_input, "may contain at most one grim rating")
-    else
-      changeset
-    end
-  end
-
   defp extract_names(tags) do
     tags
     |> Enum.map(& &1.name)
     |> MapSet.new()
   end
 
-  defp all_ratings do
-    safe_rating()
-    |> MapSet.union(sexual_ratings())
-    |> MapSet.union(horror_ratings())
-    |> MapSet.union(gross_rating())
-  end
-
-  defp safe_rating, do: MapSet.new(["safe"])
-  defp sexual_ratings, do: MapSet.new(["suggestive", "questionable", "explicit"])
-  defp horror_ratings, do: MapSet.new(["semi-grimdark", "grimdark"])
-  defp gross_rating, do: MapSet.new(["grotesque"])
 end
