5abd96337722a44028d9f092f813ec672601350a
Quavertron <user@example.com>
2026-04-18 15:52:09 -0400

    removed default tags / imgs

--- priv/repo/seeds_development.exs
+++ priv/repo/seeds_development.exs
@@ -17,127 +17,141 @@ alias Philomena.Topics
 alias Philomena.Posts
 alias Philomena.Tags
 
-{:ok, ip} = EctoNetwork.INET.cast({203, 0, 113, 0})
-{:ok, _} = Application.ensure_all_started(:plug)
+seed_dev_fixtures? =
+  System.get_env("PHILOMENA_SEED_DEV_FIXTURES", "false")
+  |> String.downcase()
+  |> then(&(&1 in ["1", "true", "yes"]))
 
-resources =
-  "priv/repo/seeds_development.json"
-  |> File.read!()
-  |> JSON.decode!()
+if seed_dev_fixtures? do
+  {:ok, ip} = EctoNetwork.INET.cast({203, 0, 113, 0})
+  {:ok, _} = Application.ensure_all_started(:plug)
 
-IO.puts "---- Generating users"
-for user_def <- resources["users"] do
-  {:ok, user} = Users.register_user(user_def)
+  resources =
+    "priv/repo/seeds_development.json"
+    |> File.read!()
+    |> JSON.decode!()
 
-  user
-  |> Repo.preload([:roles])
-  |> User.confirm_changeset()
-  |> User.update_changeset(%{role: user_def["role"]}, [])
-  |> Repo.update!()
-end
+  IO.puts("---- Generating users")
 
-pleb = Repo.get_by!(User, name: "Pleb")
-request_attributes = [
-  fingerprint: "c1836832948",
-  ip: ip,
-  user_id: pleb.id,
-  user: pleb
-]
+  for user_def <- resources["users"] do
+    {:ok, user} = Users.register_user(user_def)
 
-IO.puts "---- Generating images"
-for image_def <- resources["remote_images"] do
-  file = Briefly.create!()
-  now = DateTime.utc_now() |> DateTime.to_unix(:microsecond)
-
-  IO.puts "Fetching #{image_def["url"]} ..."
-  {:ok, %{body: body}} = PhilomenaProxy.Http.get(image_def["url"])
-
-  File.write!(file, body)
-
-  upload = %Plug.Upload{
-    path: file,
-    content_type: "application/octet-stream",
-    filename: "fixtures-#{now}"
-  }
-
-  IO.puts "Inserting ..."
-
-  Images.create_image(
-    request_attributes,
-    Map.merge(image_def, %{"image" => upload})
-  )
-  |> case do
-    {:ok, %{image: image}} ->
-      Images.approve_image(image)
-      Images.reindex_image(image)
-      Tags.reindex_tags(image.added_tags)
-
-      IO.puts "Created image ##{image.id}"
-
-    {:error, :image, changeset, _so_far} ->
-      IO.inspect changeset.errors
+    user
+    |> Repo.preload([:roles])
+    |> User.confirm_changeset()
+    |> User.update_changeset(%{role: user_def["role"]}, [])
+    |> Repo.update!()
   end
-end
 
-IO.puts "---- Generating comments for image #1"
-for comment_body <- resources["comments"] do
-  image = Images.get_image!(1)
+  pleb = Repo.get_by!(User, name: "Pleb")
 
-  Comments.create_comment(
-    image,
-    request_attributes,
-    %{"body" => comment_body}
-  )
-  |> case do
-    {:ok, %{comment: comment}} ->
-      Comments.approve_comment(comment, pleb)
-      Comments.reindex_comment(comment)
-      Images.reindex_image(image)
+  request_attributes = [
+    fingerprint: "c1836832948",
+    ip: ip,
+    user_id: pleb.id,
+    user: pleb
+  ]
 
-    {:error, :comment, changeset, _so_far} ->
-      IO.inspect changeset.errors
-  end
-end
+  IO.puts("---- Generating images")
 
-IO.puts "---- Generating forum posts"
-for %{"forum" => forum_name, "topics" => topics} <- resources["forum_posts"] do
-  forum = Repo.get_by!(Forum, short_name: forum_name)
+  for image_def <- resources["remote_images"] do
+    file = Briefly.create!()
+    now = DateTime.utc_now() |> DateTime.to_unix(:microsecond)
 
-  for %{"title" => topic_name, "posts" => [first_post | posts]} <- topics do
-    Topics.create_topic(
-      forum,
+    IO.puts("Fetching #{image_def["url"]} ...")
+    {:ok, %{body: body}} = PhilomenaProxy.Http.get(image_def["url"])
+
+    File.write!(file, body)
+
+    upload = %Plug.Upload{
+      path: file,
+      content_type: "application/octet-stream",
+      filename: "fixtures-#{now}"
+    }
+
+    IO.puts("Inserting ...")
+
+    Images.create_image(
       request_attributes,
-      %{
-        "title" => topic_name,
-        "posts" => %{
-          "0" => %{
-            "body" => first_post,
-          }
-        }
-      }
+      Map.merge(image_def, %{"image" => upload})
     )
     |> case do
-      {:ok, %{topic: topic}} ->
-        for post <- posts do
-          Posts.create_post(
-            topic,
-            request_attributes,
-            %{"body" => post}
-          )
-          |> case do
-            {:ok, %{post: post}} ->
-              Posts.approve_post(post, pleb)
-              Posts.reindex_post(post)
+      {:ok, %{image: image}} ->
+        Images.approve_image(image)
+        Images.reindex_image(image)
+        Tags.reindex_tags(image.added_tags)
 
-            {:error, :post, changeset, _so_far} ->
-              IO.inspect changeset.errors
-          end
-        end
+        IO.puts("Created image ##{image.id}")
 
-      {:error, :topic, changeset, _so_far} ->
-        IO.inspect changeset.errors
+      {:error, :image, changeset, _so_far} ->
+        IO.inspect(changeset.errors)
     end
   end
-end
 
-IO.puts "---- Done."
+  IO.puts("---- Generating comments for image #1")
+
+  for comment_body <- resources["comments"] do
+    image = Images.get_image!(1)
+
+    Comments.create_comment(
+      image,
+      request_attributes,
+      %{"body" => comment_body}
+    )
+    |> case do
+      {:ok, %{comment: comment}} ->
+        Comments.approve_comment(comment, pleb)
+        Comments.reindex_comment(comment)
+        Images.reindex_image(image)
+
+      {:error, :comment, changeset, _so_far} ->
+        IO.inspect(changeset.errors)
+    end
+  end
+
+  IO.puts("---- Generating forum posts")
+
+  for %{"forum" => forum_name, "topics" => topics} <- resources["forum_posts"] do
+    forum = Repo.get_by!(Forum, short_name: forum_name)
+
+    for %{"title" => topic_name, "posts" => [first_post | posts]} <- topics do
+      Topics.create_topic(
+        forum,
+        request_attributes,
+        %{
+          "title" => topic_name,
+          "posts" => %{
+            "0" => %{
+              "body" => first_post
+            }
+          }
+        }
+      )
+      |> case do
+        {:ok, %{topic: topic}} ->
+          for post <- posts do
+            Posts.create_post(
+              topic,
+              request_attributes,
+              %{"body" => post}
+            )
+            |> case do
+              {:ok, %{post: post}} ->
+                Posts.approve_post(post, pleb)
+                Posts.reindex_post(post)
+
+              {:error, :post, changeset, _so_far} ->
+                IO.inspect(changeset.errors)
+            end
+          end
+
+        {:error, :topic, changeset, _so_far} ->
+          IO.inspect(changeset.errors)
+      end
+    end
+  end
+
+  IO.puts("---- Done.")
+else
+  IO.puts("---- Skipping development fixtures (set PHILOMENA_SEED_DEV_FIXTURES=true to enable)")
+end
